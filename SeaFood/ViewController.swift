a45a04a733b2d367c0d7510aad7d0832cb92a870
Badal Aryal <badal.aryal@gmail.com>
2024-04-05 11:08:18 +0545

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Badal  Aryal on 05/04/2024.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
