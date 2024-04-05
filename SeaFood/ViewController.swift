72aaf41e5e6c8d43e9cfd7127351dddf72c67164
Badal Aryal <badal.aryal@gmail.com>
2024-04-05 12:47:13 +0545

    Enable camera functionality of app

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -10,14 +10,37 @@ import CoreML
 import Vision
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
+    
+    // what should happen once the user has picked that image
+    // delegate method
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            // set the imageview in the background to that image that they've picked
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
