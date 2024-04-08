b6b82a0b4bb4996f83b06e465f3a9114f24f3a87
Badal Aryal <badal.aryal@gmail.com>
2024-04-08 15:38:45 +0545

    Add hotdog or not hotdog functionality in Navigation bar.

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -53,7 +53,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results = request.results as? [VNClassificationObservation] else{
                 fatalError("Model failed to process image")
             }
-            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
         }
         //handler that specifies the image we want to classify
         let handler = VNImageRequestHandler(ciImage: image)
